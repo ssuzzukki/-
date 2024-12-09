f68010fc74e3512dd2f12d01b560366529adf62c
ssuzzukki <yuga.3kan@gmail.com>
2024-12-09 16:58:48 +0900

    pita.rbを改良

--- pita.rb
+++ pita.rb
@@ -1,29 +1,32 @@
-#p 999917%4
-##p 999931%4
-#p 999953%4
-##p 999959%4
-#p 999961%4
 #ピタゴラス素数の判定
 
 
 def _pita(n)
-    for i in 1 ..n do
-        n2 = n - i*i
-        if n2 < 0
-            return []
-        end
-        for j in 1 ..n2 do
-            if n2 == j*j
-                ans = [i, j]
-                return ans
+    ans = []
+    if n % 4 == 1
+        for i in 1 ..n do
+            n2 = n - i*i
+            if n2 < 0
+                #return []
+            end
+            for j in 1 ..n2 do
+                if n2 == j*j
+                    ans = [i, j]
+                    #return ans
+                end
             end
         end
+        if ans.size == 2
+            puts "#{n}はピタゴラス素数です"
+            puts "2つの平方数の根は#{ans[0]},#{ans[1]}です"
+        else
+            puts "#{n}はピタゴラス素数ではありません"
+        end
+    else
+        puts "#{n}はピタゴラス素数ではありません"
     end
 end
 
 puts "number?"
 n = gets.to_i
-
-a = _pita(n)
-p a
-p a[0]**2 + a[1]**2
+_pita(n)
